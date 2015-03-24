575204ab9192b85725e4107e7bc71c404a2be6a5
AmiralSAS <sylvain.schmitt@syceya.com>
2015-03-24 09:06:39 +0100

    reorganized repository

---
+++ second prototype/js/prototype_interface.js
@@ -0,0 +1,35 @@
+function handleSingleFile(evt) {
+	var files = evt.target.files; // FileList object
+	var filenames = [];
+	var audiofiles = [];
+	for (var i = 0, f; f = files[i]; i++) {
+		filenames.push( encodeURIComponent(f.name) );
+		audiofiles.push( f.name );
+		console.log("eee", f.mozFullPath);
+	}
+	var afilereader = new FileReader();
+	afilereader.onload = function(e) {
+    	music_player = document.createElement("audio");
+    	music_player.src = e.target.result;
+    	music_player.play();
+	}
+	afilereader.readAsDataURL(audiofiles[0]);
+
+
+	createTable({
+		containerId: "musicTableContainer",
+		tableId: "musicMapper",
+    	type: "music",
+		filenames: filenames
+	});
+	console.log(evt.target);
+	console.log(evt.target.files[0].name);
+}
+document.getElementById('key_a').addEventListener('change', handleSingleFile, false);
+
+
+
+
+
+
+console.log("prototype_interface.js loaded");
