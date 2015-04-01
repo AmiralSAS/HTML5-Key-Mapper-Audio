786523f175083c193dc4ff07eaba5b650809e915
AmiralSAS <sylvain.schmitt@syceya.com>
2015-04-01 09:49:59 +0200

    proto recognize key press

--- second prototype/js/prototype_interface.js
+++ second prototype/js/prototype_interface.js
@@ -1,28 +1,78 @@
-function handleSingleFile(evt) {
-	var files = evt.target.files; // FileList object
-	var filenames = [];
-	var audiofiles = [];
+ /*
+ * Prototype 2 - Interface
+*/
+var datas = {};
+datas.key_list = [];
+datas.music_player = document.createElement("audio");
+datas.sound_list = {};
+datas.sound_active = [];
+
+function handleFileSelect(evt) {
+	var files = evt.target.files;
+	var actualFileInput = this;
+
 	for (var i = 0, f; f = files[i]; i++) {
-		filenames.push( encodeURIComponent(f.name) );
-		audiofiles.push( f.name );
-		console.log("eee", f.mozFullPath);
-	}
-	var afilereader = new FileReader();
-	afilereader.onload = function(e) {
-    	music_player = document.createElement("audio");
-    	music_player.src = e.target.result;
-    	music_player.play();
-	}
-	afilereader.readAsDataURL(audiofiles[0]);
+		if (!f.type.match('audio.*')) {
+			continue;
+		}
 
-	console.log(evt.target);
-	console.log(evt.target.files[0].name);
+		var reader = new FileReader();
+		// Closure to capture the file information.
+		reader.onload = (function(theFile) {
+			return function(e) {
+				datas.key_list[actualFileInput.getAttribute("data-keylabel")] = {
+					source : e.target.result
+				};
+			};
+		})(f);
+
+		reader.readAsDataURL(f);
+	}
 }
-document.getElementById('key_a').addEventListener('change', handleSingleFile, false);
+
+ /***********************
+ * Listening to keyboard
+*/
+var classname = document.getElementsByClassName("key_fileinput");
+
+for(var i=0;i<classname.length;i++){
+	classname[i].addEventListener('change', handleFileSelect, false);
+}
+
+ /****************
+ * Some functions
+*/
+function addKey(){
+	/*
+	var last_index = datas.sound_active.length;
+	var sound_player = document.createElement("audio");
+	datas.active_sounds.push(sound_player);
+	datas.active_sounds[last_index].src = decodeURIComponent(fullpath);
+	*/
+}
 
 
 
+function playMusic(theKeyToPlay){
+	datas.music_player.src = datas.key_list[theKeyToPlay].source;
+	datas.music_player.play();
+}
 
 
 
-console.log("prototype_interface.js loaded");
+function manageKeyPressure(){
+	var inputKeys = document.getElementsByClassName('key_fileinput');
+
+	for (var i=0; i < inputKeys.length; i++) {
+		if (key.isPressed( inputKeys[i].getAttribute("data-keylabel") )) {
+			console.log( inputKeys[i].getAttribute("data-keylabel") );
+			// Animate function
+
+			// play music function
+			
+		}
+
+	}
+}
+
+document.onkeydown = manageKeyPressure;
