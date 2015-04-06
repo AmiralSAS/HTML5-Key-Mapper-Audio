a918c7efae10c8740c84ac64cd3b78a4575e94f2
AmiralSAS <sylvain.schmitt@syceya.com>
2015-04-06 20:49:54 +0200

    now numbers play musics and letters play sounds

--- second prototype/js/prototype_interface.js
+++ second prototype/js/prototype_interface.js
@@ -1,12 +1,17 @@
  /*
  * Prototype 2 - Interface
 */
+
 var datas = {};
 datas.key_list = [];
 datas.music_player = document.createElement("audio");
-datas.sound_list = {};
-datas.sound_active = [];
+datas.active_sounds = [];
 
+    /*****************
+   ******************
+  ** rag and drop manager
+ ******************
+*****************/ 
 function handleFileSelect(evt) {
 	var files = evt.target.files;
 	var actualFileInput = this;
@@ -19,7 +24,7 @@ function handleFileSelect(evt) {
 
 		var reader = new FileReader();
 		datas.key_list[this_key] = {};
-		datas.key_list[this_key].name = encodeURIComponent(files[i].name);
+		datas.key_list[this_key].name = encodeURIComponent(files[i].name); // safely stock the filename
 		datas.key_list[this_key].key = this_key;
 
 		// Closure to capture the file information.
@@ -35,31 +40,47 @@ function handleFileSelect(evt) {
 	}
 }
 
- /***********************
- * Listening to keyboard
-*/
+    /*****************
+   ******************
+  ** Keyboard listening
+ ******************
+*****************/ 
+// This for listening for every change of each input files
 var classname = document.getElementsByClassName("key_fileinput");
-
 for(var i=0;i<classname.length;i++){
 	classname[i].addEventListener('change', handleFileSelect, false);
 }
+// This for listening to spacebar
+document.onkeydown = manageKeyPressure;
 
- /****************
- * Some functions
-*/
-function playKey(theKeyPressed){
-	if (datas.key_list[theKeyPressed]) {
-		playMusic(theKeyPressed);
+    /*****************
+   ******************
+  ** Keymap
+ ******************
+*****************/ 
+function manageKeyPressure(){
+	var inputKeys = document.getElementsByClassName('key_fileinput');
+
+	for (var i=0; i < inputKeys.length; i++) {
+		if (key.isPressed( inputKeys[i].getAttribute("data-keylabel") )) {
+			// Animate function
+
+			// play music function
+			playKey( inputKeys[i].getAttribute("data-keylabel") );
+		}
+
 	}
 }
 
-function playMusic(theKeyToPlay){
-	datas.music_player.src = datas.key_list[theKeyToPlay].source;
-	datas.music_player.play();
-}
-
-function playSound(){
-
+function playKey(theKeyPressed){
+	if (datas.key_list[theKeyPressed]) {
+		console.log(datas.key_list[theKeyPressed].key);
+		if (isNaN(datas.key_list[theKeyPressed].key)) {
+			playSound(theKeyPressed);
+		} else {
+			playMusic(theKeyPressed);
+		}
+	}
 }
 
 function updateKeymap(){
@@ -79,19 +100,52 @@ function updateKeymap(){
 	};
 }
 
+key("space", stopAll);
 
-function manageKeyPressure(){
-	var inputKeys = document.getElementsByClassName('key_fileinput');
 
-	for (var i=0; i < inputKeys.length; i++) {
-		if (key.isPressed( inputKeys[i].getAttribute("data-keylabel") )) {
-			// Animate function
-
-			// play music function
-			playKey( inputKeys[i].getAttribute("data-keylabel") );
-		}
-
-	}
+    /*****************
+   ******************
+  ** Sound scripts
+ ******************
+*****************/ 
+ /*
+ * Lecture play functions
+*/
+function playMusic(theKeyToPlay){
+	console.log("music");
+	datas.music_player.src = datas.key_list[theKeyToPlay].source;
+	datas.music_player.play();
 }
 
-document.onkeydown = manageKeyPressure;
+function playSound(theKeyToPlay){
+	console.log("sound");
+    var last_index = datas.active_sounds.length;
+    var sound_player = document.createElement("audio");
+    datas.active_sounds.push(sound_player);
+    datas.active_sounds[last_index].src = datas.key_list[theKeyToPlay].source;
+    datas.active_sounds[last_index].play();
+}
+
+ /*
+ * Lecture stop functions
+*/
+function stopAll(){
+    stopMusic();
+    stopSounds();
+}
+function stopMusic(){
+    if (datas.music_player.src) {
+        datas.music_player.src = "";
+        datas.music_player.load();
+    };
+}
+function stopSounds(){
+    if (datas.active_sounds.length > 0) {
+        // destroy the array containing the sounds elements to stop them from playing
+        for (var i = datas.active_sounds.length - 1; i >= 0; i--) {
+            datas.active_sounds[i].src = "";
+            datas.active_sounds[i].load();
+            datas.active_sounds.pop();
+        };
+    };
+}
